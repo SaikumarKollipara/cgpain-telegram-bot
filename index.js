3d6229f01abc76adf9c2ca9b402a6d2ffdad0d12
SaikumarKollipara <krksaikumar@gmail.com>
2023-03-15 16:16:50 +0530

    Added webhook

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import TelegramBot from 'node-telegram-bot-api' 
+// import TelegramBot from 'node-telegram-bot-api'
 // import dotenv from "dotenv";
 // dotenv.config();
 
@@ -11,26 +11,25 @@
 //     const rollNo = match[1];
 //     const response = `Results of${rollNo}:
 
-// 1-1 Sem : 9.1  
-// 1-2 Sem : 8.9  
-// 2-1 Sem : 8.7  
-// 2-2 Sem : 7.5  
-// 3-1 Sem : 8.1  
-// 3-2 Sem : 7.2  
-// 4-1 Sem : 8.0  
-// 4-2 Sem : 9.4  
+// 1-1 Sem : 9.1
+// 1-2 Sem : 8.9
+// 2-1 Sem : 8.7
+// 2-2 Sem : 7.5
+// 3-1 Sem : 8.1
+// 3-2 Sem : 7.2
+// 4-1 Sem : 8.0
+// 4-2 Sem : 9.4
 //     `;
 //     bot.sendMessage(chatId, response);
 // });
 
-
-import TelegramBot from 'node-telegram-bot-api';
-import express from 'express';
+import TelegramBot from "node-telegram-bot-api";
+import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const token = process.env.BOT_TOKEN;
 
@@ -38,11 +37,10 @@ const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token);
 
 // Listen for incoming messages
-bot.on('message', (msg, match) => {
-
-    const chatId = msg.chat.id;
-    const rollNo = match[1];
-    const response = `Results of${rollNo}:
+bot.onText(/\/getresults(.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const rollNo = match[1];
+  const response = `Results of${rollNo}:
 
 1-1 Sem : 9.1  
 1-2 Sem : 8.9  
@@ -53,11 +51,11 @@ bot.on('message', (msg, match) => {
 4-1 Sem : 8.0  
 4-2 Sem : 9.4  
     `;
-    bot.sendMessage(chatId, response);
+  bot.sendMessage(chatId, response);
 });
 
-app.get('/', (req, res)=>{
-    res.send({ message: 'Telegram bot for CGPAIN' });
+app.get("/", (req, res) => {
+  res.send({ message: "Telegram bot for CGPAIN" });
 });
 
 // Set up a webhook route
@@ -73,6 +71,9 @@ app.listen(PORT, () => {
 
 // Set the webhook for the bot
 bot.setWebHook({
-  url: process.env.URL + `/bot${token}`
+    url: process.env.URL + `/bot${token}`,
+}).then(() => {
+    console.log("Webhook set successfully");
+}).catch((err) => {
+    console.error("Error setting webhook", err);
 });
-
