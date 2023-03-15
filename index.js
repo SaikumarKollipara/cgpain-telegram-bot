940b115124f76f1d54b5ca9b8353b2bce91df4bb
SaikumarKollipara <krksaikumar@gmail.com>
2023-03-15 16:23:38 +0530

    Added webhook

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// import TelegramBot from 'node-telegram-bot-api'
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const token = process.env.BOT_TOKEN;
-// const bot = new TelegramBot(token, { polling: true });
-
-// bot.onText(/\/getresults(.+)/, (msg, match) => {
-
-//     const chatId = msg.chat.id;
-//     const rollNo = match[1];
-//     const response = `Results of${rollNo}:
-
-// 1-1 Sem : 9.1
-// 1-2 Sem : 8.9
-// 2-1 Sem : 8.7
-// 2-2 Sem : 7.5
-// 3-1 Sem : 8.1
-// 3-2 Sem : 7.2
-// 4-1 Sem : 8.0
-// 4-2 Sem : 9.4
-//     `;
-//     bot.sendMessage(chatId, response);
-// });
-
 import TelegramBot from "node-telegram-bot-api";
 import express from "express";
 import dotenv from "dotenv";
@@ -34,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const token = process.env.BOT_TOKEN;
 
 // Create a bot instance
-const bot = new TelegramBot(token);
+const bot = new TelegramBot(token, { polling: true });
 
 // Listen for incoming messages
 bot.onText(/\/getresults(.+)/, (msg, match) => {
