59609073a4a75f618e5084683c9fcc6c5bffbc94
SaikumarKollipara <krksaikumar@gmail.com>
2023-03-23 18:05:49 +0530

    Changed code structure

--- index.js
+++ index.js
@@ -13,22 +13,24 @@ const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token);
 
 // Listen for incoming messages
-bot.onText(/\/getresults(.+)/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const rollNo = match[1];
-    const response = `Results of${rollNo}:
+bot.onText(/.*/, msg => {
+    const message = msg.text;
+    const chatID = msg.chat.id;
+    console.log('From:', msg.from.first_name, msg.from.last_name);
+    console.log('Message:', message);
+    let response;
+    if (/\/start/.test(message)) {
+        response = 'Hello, welcome to CGPA IN BOT 👋 \nTry entering your roll number to get the results of all semesters.\nTry \\help to get the list of all commands.';
+        
+    } else if (/^\d{3}\w\d\w\d{4}$/.test(message)) {
+        // Matches rollNo
+        response = `Results of ${message}: \n\n1-1 Sem : 9.1\n1-2 Sem : 8.9 \n2-1 Sem : 8.7 \n2-2 Sem : 7.5 \n3-1 Sem : 8.1 \n3-2 Sem : 7.2 \n4-1 Sem : 8.0 \n4-2 Sem : 9.4`
 
-1-1 Sem : 9.1  
-1-2 Sem : 8.9  
-2-1 Sem : 8.7  
-2-2 Sem : 7.5  
-3-1 Sem : 8.1  
-3-2 Sem : 7.2  
-4-1 Sem : 8.0  
-4-2 Sem : 9.4  
-    `;
-    bot.sendMessage(chatId, response);
-});
+    } else {
+        response = 'Sorry, I didn\'t understand that.';
+    }
+    bot.sendMessage(chatID, response);
+})
 
 app.use(bodyParser.json())
 
@@ -38,7 +40,6 @@ app.get("/", (req, res) => {
 
 // Set up a webhook route
 app.post(`/bot${token}`, (req, res) => {
-    console.log('req body', req.body)
     bot.processUpdate(req.body);
     res.sendStatus(200);
 });
